04cddc26051b29eb2a6bbb133145885c6ddaf1dd
Emil <emil88pl@gmail.com>
2023-03-06 19:10:56 +0000

    GUI Simple

--- src/GUI.java
+++ src/GUI.java
@@ -3,30 +3,56 @@ import java.awt.BorderLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+
+
+
+public class GUI implements ActionListener {
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+    
     public GUI() {
-        JFrame frame = new JFrame();
-
+        frame = new JFrame();
+    
         JButton button = new JButton("Click me");
-
-        JPanel panel = new JPanel();
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: 0");
+    
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(button);
+    
+        panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
-        panel.setLayout(new java.awt.BorderLayout());
-        panel.add(button);
-
+        panel.setLayout(new BorderLayout());
+        panel.add(buttonPanel, BorderLayout.NORTH);
+        panel.add(label, BorderLayout.CENTER);
+    
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Emil GUI");
         frame.pack();
         frame.setVisible(true);
     }
+    
 
 
     public static void main(String[] args) throws Exception {
         new GUI();
 
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count );
     }
 }
